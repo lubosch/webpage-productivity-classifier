299c89ecf936412386ff9ebae904019e195561c2
lubosch <lubomir.vnenk@zoho.com>
2015-10-29 04:44:25 +0100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -93,4 +93,16 @@ namespace :deploy do
 
 end
 
+namespace :bower do
+  desc 'Install bower'
+  task :install do
+    on roles(:web) do
+      within release_path do
+        execute :rake, 'bower:install CI=true'
+      end
+    end
+  end
+end
+before 'deploy:compile_assets', 'bower:install'
+
 after 'deploy:publishing', 'deploy:restart'
